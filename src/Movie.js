7b9c0cdae6f8fbfcff378331ab177dd8f7230377
samdae3@naver.com <samdae3@naver.com>
2021-04-08 23:13:54 +0900

    SI dauhun_lee 신규파일

---
+++ src/Movie.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movies__movie">
+            <img src={poster} art={title} title={title}></img>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genres, index) => 
+                        <li key={index} className="genres__genre">
+                            {genres}
+                        </li>)
+                    }
+                </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
